004f802c4ac4f3688730b8e8b0739062335b8f19
Maryia Rasulava <m.rasulava@gmail.com>
2016-08-12 01:16:37 +0300

    Added weight screen

--- app/src/main/java/com/udacity/rasulava/capstone_project/AddProductFragment.java
+++ app/src/main/java/com/udacity/rasulava/capstone_project/AddProductFragment.java
@@ -10,6 +10,8 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.udacity.rasulava.capstone_project.db.model.Product;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -30,6 +32,9 @@ public class AddProductFragment extends DialogFragment {
 
     @BindView(R.id.et_prot)
     EditText etProt;
+
+    @BindView(R.id.et_kcal)
+    EditText etKcal;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -56,14 +61,32 @@ public class AddProductFragment extends DialogFragment {
         } else if (etProt.getText().toString().length() == 0) {
             etProt.requestFocus();
             etProt.setError(getString(R.string.error_empty_value));
+        } else if (etKcal.getText().toString().length() == 0) {
+            etKcal.requestFocus();
+            etKcal.setError(getString(R.string.error_empty_value));
         } else {
             int fat = Integer.parseInt(etFat.getText().toString());
             int carb = Integer.parseInt(etCarb.getText().toString());
             int prot = Integer.parseInt(etProt.getText().toString());
             if (fat + carb + prot > 100)
                 Toast.makeText(getActivity(), "The total weight should not exceed 100 gr!", Toast.LENGTH_LONG).show();
-            else
+            else {
+                Product product = new Product();
+                product.setName(etName.getText().toString());
+                product.setCalories(etKcal.getText().toString());
+                product.setFat(etFat.getText().toString());
+                product.setCarbohydrate(etCarb.getText().toString());
+                product.setProtein(etProt.getText().toString());
+
+                ProductWeightDialog dialog = new ProductWeightDialog();
+
+                Bundle args = new Bundle();
+                args.putSerializable("product", product);
+                dialog.setArguments(args);
+
+                dialog.show(getFragmentManager(), "tag1");
                 dismiss();
+            }
         }
     }
 
---
+++ app/src/main/java/com/udacity/rasulava/capstone_project/ProductWeightDialog.java
@@ -0,0 +1,56 @@
+package com.udacity.rasulava.capstone_project;
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.udacity.rasulava.capstone_project.db.model.Product;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * Created by Maryia on 12.08.2016.
+ */
+public class ProductWeightDialog extends DialogFragment {
+
+    @BindView(R.id.tv_title)
+    TextView tvTitle;
+
+    @BindView(R.id.et_weight)
+    EditText etWeight;
+
+    private Product product;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.dialog_product_weight, null);
+        ButterKnife.bind(this, rootView);
+        product = (Product) getArguments().getSerializable("product");
+        tvTitle.setText(getString(R.string.weight_dialog_title, product.getName()));
+        return rootView;
+    }
+
+
+    @OnClick(R.id.btn_save)
+    public void saveProduct(View view) {
+        if (etWeight.getText().toString().length() == 0) {
+            etWeight.requestFocus();
+            etWeight.setError(getString(R.string.error_empty_value));
+        } else {
+            int weight = Integer.parseInt(etWeight.getText().toString());
+            dismiss();
+        }
+
+    }
+
+    @OnClick(R.id.btn_cancel)
+    public void cancel(View view) {
+        dismiss();
+    }
+}
